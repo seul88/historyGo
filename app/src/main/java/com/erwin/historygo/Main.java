1a2e5bd03a755f384306d34e9bab619431298768
Erwin Majewski <erwin.majewski@gmail.com>
2018-06-01 18:59:59 +0200

    New activities layout fix

--- app/src/main/java/com/erwin/historygo/Main.java
+++ app/src/main/java/com/erwin/historygo/Main.java
@@ -41,9 +41,20 @@ public class Main extends AppCompatActivity {
                 Intent intent3 = new Intent(this, Places.class);
                 this.startActivity(intent3);
                 return true;
+
             case R.id.activity_malta:
                 Intent intent4 = new Intent(this, Malta.class);
                 this.startActivity(intent4);
+                return true;
+
+            case R.id.activity_politechnika:
+                Intent intent5 = new Intent(this, Politechnika.class);
+                this.startActivity(intent5);
+                return true;
+
+            case R.id.activity_ratusz:
+                Intent intent6 = new Intent(this, Ratusz.class);
+                this.startActivity(intent6);
                 return true;
 
             default:
--- app/src/main/java/com/erwin/historygo/Politechnika.java
+++ app/src/main/java/com/erwin/historygo/Politechnika.java
@@ -9,5 +9,7 @@ public class Politechnika extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_politechnika);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
